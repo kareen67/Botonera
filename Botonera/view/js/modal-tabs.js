b8711cd3ac99c8cb2c6bbdf0bbb90cb3bd661a7b
kareen67 <karomi.edu@gmail.com>
2025-11-09 20:00:14 -0300

    vista reproducir operador , creo q ya son todas

--- Botonera/view/js/modal-tabs.js
+++ Botonera/view/js/modal-tabs.js
@@ -6,17 +6,19 @@ document.getElementById("closeModal").onclick = () => {
     document.getElementById("modal").style.display = "none";
 };
 
+
 // Tabs
 const tabs = document.querySelectorAll('.tab');
-const grids = document.querySelectorAll('.fx-grid, .fx-tabla, .fx-inst');
+const grids = document.querySelectorAll('.fx-grid');
 
-if (tabs.length && grids.length) {
-    tabs.forEach(tab => {
-        tab.addEventListener('click', () => {
-            tabs.forEach(t => t.classList.remove('active'));
-            tab.classList.add('active');
-            grids.forEach(grid => grid.classList.add('hidden'));
-            document.getElementById(tab.dataset.target).classList.remove('hidden');
-        });
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        // quitar active a todas
+        tabs.forEach(t => t.classList.remove('active'));
+        tab.classList.add('active');
+        // ocultar todas las grids
+        grids.forEach(grid => grid.classList.add('hidden'));
+        // mostrar la grid correspondiente
+        document.getElementById(tab.dataset.target).classList.remove('hidden');
     });
-}
+});
