616992b2d2ff7339f20f7f7cdd037d8dde798be1
kareen67 <karomi.edu@gmail.com>
2025-11-10 15:15:00 -0300

    .

---
+++ Botonera/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenida</title>
+
+    <!-- Fuente e iconos -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="./view/css/Global.css">
+    <link rel="stylesheet" href="./view/css/header.css">
+    <link rel="stylesheet" href="./view/css/fondo-musical.css">
+
+</head>
+
+<body>
+     <div class="musical-background">
+        <span>♪</span>
+        <span>♫</span>
+        <span>♬</span>
+        <span>♩</span>
+        <span>♭</span>
+        <span>♮</span>
+        <span>♯</span>
+        <span>♬</span>
+        <span>♪</span>
+        <span>♩</span>
+        <span>♫</span>
+        <span>♭</span>
+    </div>
+    <section class="bienvenida">
+        <div class="contenedor">
+            <h1><i class="fa-solid fa-music"></i> Bienvenido a la Botonera FX</h1>
+            <p>Gestioná tus efectos de sonido de forma rápida y simple.</p>
+            <button class="ingresarr" > 
+                <a href="./view/login.php"> <i class="fa-solid fa-right-to-bracket"></i> Ingresar </a>
+            </button>
+        </div>
+    </section>
+</body>
+
+</html>
